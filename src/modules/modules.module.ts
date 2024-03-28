81846a3dbff589456c5efe15c58e69cf588f3622
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-28 16:16:21 +0500

    refactor: connect homepage module

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 
 import { FileModule } from "./file/file.module";
 import { UserModule } from "./user/user.module";
-import { TopicModule } from "./topic/topic.module";
+import { HomepageModule } from "./homepage/homepage.module";
+import { SectionModule } from "./section/section.module";
 import { ArticleModule } from "./article/article.module";
 import { AdminModule } from "./admin/admin.module";
 
 @Module({
-  imports: [FileModule, UserModule, TopicModule, ArticleModule, AdminModule],
+  imports: [FileModule, UserModule, HomepageModule, SectionModule, ArticleModule, AdminModule],
 })
 export class Modules {}
