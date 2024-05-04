a7f9e84b68a25a00c52fff9904ba661890e9d967
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-05 02:47:29 +0500

    refactor: remove cron-test module

--- src/modules/cron-test/cron-test.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { CronTestController } from "./cron-test.controller";
-
-@Module({
-  imports: [],
-  controllers: [CronTestController],
-})
-export class CronTestModule {}
