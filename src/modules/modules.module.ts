fabdad13fb8fd282632d2188d568583f5e7f9c12
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-05 02:47:07 +0500

    refactor: add header module and remove cron module

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -2,13 +2,13 @@ import { Module } from "@nestjs/common";
 
 import { FileModule } from "./file/file.module";
 import { UserModule } from "./user/user.module";
+import { HeaderModule } from "./header/header.module";
 import { HomepageModule } from "./homepage/homepage.module";
 import { SectionModule } from "./section/section.module";
 import { ArticleModule } from "./article/article.module";
 import { AdminModule } from "./admin/admin.module";
-import { CronTestModule } from "./cron-test/cron-test.module";
 
 @Module({
-  imports: [FileModule, UserModule, HomepageModule, SectionModule, ArticleModule, AdminModule, CronTestModule],
+  imports: [FileModule, UserModule, HeaderModule, HomepageModule, SectionModule, ArticleModule, AdminModule],
 })
 export class Modules {}
