4b2588f57a517f3f9c94e584e7ccbc16ffc8c2d2
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-24 17:08:29 +0500

    refactor: connect sponsor module

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -6,9 +6,25 @@ import { HeaderModule } from "./header/header.module";
 import { HomepageModule } from "./homepage/homepage.module";
 import { SectionModule } from "./section/section.module";
 import { ArticleModule } from "./article/article.module";
+import { DateModule } from "./date/date.module";
+import { FeeModule } from "./fee/fee.module";
+import { SponsorModule } from "./sponsor/sponsor.module";
+import { SubmissionModule } from "./submission/submission.module";
 import { AdminModule } from "./admin/admin.module";
 
 @Module({
-  imports: [FileModule, UserModule, HeaderModule, HomepageModule, SectionModule, ArticleModule, AdminModule],
+  imports: [
+    FileModule,
+    UserModule,
+    HeaderModule,
+    HomepageModule,
+    DateModule,
+    FeeModule,
+    SponsorModule,
+    SubmissionModule,
+    SectionModule,
+    ArticleModule,
+    AdminModule,
+  ],
 })
 export class Modules {}
